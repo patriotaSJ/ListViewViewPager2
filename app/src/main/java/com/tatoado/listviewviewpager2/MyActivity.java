82733ec67226175fbb18b603a44d0e1ff147e73c
ramiromadriaga <ramiro.madriaga@gmail.com>
2014-08-13 23:31:16 -0300

    ViewPager mas ListView
    una galeria de fotos con un listView abajo

---
+++ app/src/main/java/com/tatoado/listviewviewpager2/MyActivity.java
@@ -0,0 +1,124 @@
+package com.tatoado.listviewviewpager2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+
+public class MyActivity extends ActionBarActivity {
+
+    String[] ropa = {
+            "ropfdsfdsfsda1",
+            "ropa2",
+            "ropa3",
+            "ropa4"
+    };
+
+    int[] imagenes = {
+            R.drawable.tat1,
+            R.drawable.tat2,
+            R.drawable.tat3
+    };
+
+    ManejadoraGaleria manejadorGaleria;
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        //lista
+        ListView lista = (ListView) findViewById(R.id.listView1);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ropa );
+        lista.setAdapter(adapter);
+
+        //galeria de imagenes
+
+        manejadorGaleria = new ManejadoraGaleria(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        manejadorGaleria.agregarFragmentos(FragmentosImagenes.newInstance(imagenes[0]));
+        manejadorGaleria.agregarFragmentos(FragmentosImagenes.newInstance(imagenes[1]));
+        manejadorGaleria.agregarFragmentos(FragmentosImagenes.newInstance(imagenes[2]));
+
+        mViewPager.setAdapter(manejadorGaleria);
+
+    }
+
+
+
+    public class ManejadoraGaleria extends FragmentPagerAdapter {
+
+        List<Fragment> fragmentos;
+        public ManejadoraGaleria(FragmentManager fm) {
+            super(fm);
+            fragmentos = new ArrayList<Fragment>();
+        }
+
+        public void agregarFragmentos(Fragment xfragmento){
+            fragmentos.add(xfragmento);
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentos.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentos.size();
+        }
+    }
+
+
+    public static class FragmentosImagenes extends Fragment {
+
+        private static final String ARG_IMAGE = "imagen";
+        private int imagen;
+
+        public static FragmentosImagenes newInstance(int imagen) {
+            FragmentosImagenes fragment = new FragmentosImagenes();
+            Bundle args = new Bundle();
+            args.putInt(ARG_IMAGE, imagen);
+            fragment.setArguments(args);
+            fragment.setRetainInstance(true);
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            if(getArguments() != null) {
+                imagen = getArguments().getInt(ARG_IMAGE);
+            }
+        }
+
+        public FragmentosImagenes() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+
+            ImageView imagenView = (ImageView) rootView.findViewById(R.id.imageView1);
+            imagenView.setImageResource(imagen);
+            return rootView;
+        }
+    }
+
+}
